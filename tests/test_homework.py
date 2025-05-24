8b4c17c175e0ea6e6f18390996469fddc6d9539f
Alejandro Cartagena <acartagena@unal.edu.co>
2025-05-24 12:31:00 -0500

    Update test_homework.py

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -14,7 +14,6 @@ def test_01():
         subprocess.run(
             [
                 "mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5"
-"
             ],
             check=True,
         )
